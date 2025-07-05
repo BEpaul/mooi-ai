7c3d8b28f89a127e1e0e98f625f1d3ae67c5cb4c
Jinhyuk-Yang <yangkyforever@gmail.com>
2025-07-05 23:55:27 +0900

    feat: analysis is not a chat

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from prompt.sentiment import (
 )
 
 load_dotenv()
-print(SENTIMENT_OUTPUT_PARSER.get_format_instructions())
 
 # chat view
 chat_box = ChatBox(
@@ -26,6 +25,19 @@ chat_box = ChatBox(
 
 chat_box.init_session()
 chat_box.output_messages()
+
+if "sentiment_output" not in st.session_state:
+    st.session_state["sentiment_output"] = ""
+
+def get_history_chats():
+    print(chat_box.history)
+    return [
+        HistoryChat(role=elem['role'], message=str(elem['elements'][0]))
+        for elem in chat_box.history
+    ]
+
+# language model
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 
 with st.sidebar:
     st.subheader('Prompt Setting')
@@ -38,15 +50,12 @@ with st.sidebar:
         value=DEFAULT_SENTIMENT_PROMPT_TEMPLATE_MESSAGE
     )
 
-def get_history_chats():
-    return [
-        HistoryChat(role=elem['role'], message=str(elem['elements'][0]))
-        for elem in chat_box.history
-    ]
+    if st.button("전체 대화 감성 분석"):
+        full_dialogue = "\n".join([history_chat.to_message() for history_chat in get_history_chats()])
+        sentiment_chain = make_sentiment_prompt_template(analyze_prompt_message) | llm | SENTIMENT_OUTPUT_PARSER
+        result = sentiment_chain.invoke({"input": full_dialogue})
+        st.session_state["sentiment_output"] = f"분류: {result.sentiment}\n\n이유: {result.reason}"
 
-# language model
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
-sentiment_chain = make_sentiment_prompt_template(analyze_prompt_message) | llm | SENTIMENT_OUTPUT_PARSER
 
 if user_input := st.chat_input('당신의 마음을 표현하세요'):
     chat_box.user_say(user_input)
@@ -60,19 +69,9 @@ if user_input := st.chat_input('당신의 마음을 표현하세요'):
     answer = chat_result.content
     chat_box.ai_say(Markdown(answer, title="답변", in_expander=True, expanded=True))
 
-if st.button("감성 분석하기"):
-    full_dialogue = "\n".join([history_chat.to_message() for history_chat in get_history_chats()])
-    
-    # analyze sentiment
-    result = sentiment_chain.invoke({"input": full_dialogue})
-    sentiment = result.sentiment
-    reason = result.reason
-    
-    chat_box.ai_say(
-        Markdown(
-            f'전체 대화 감성 분석 결과\n\n분류: {sentiment}\n\n이유: {reason}',
-            title="종합 감성 분석",
-            in_expander=True,
-            expanded=True
-        )
+if st.session_state["sentiment_output"]:
+    st.subheader("감성 분석 결과")
+    st.text_area(
+        value=st.session_state["sentiment_output"],
+        height=150
     )
