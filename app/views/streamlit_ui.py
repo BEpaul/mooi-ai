074951a0c3cf0397c90310aaf5abae9db5877c01
Jinhyuk-Yang <yangkyforever@gmail.com>
2025-07-15 15:45:24 +0900

    refactor: minor

--- app/views/streamlit_ui.py
+++ app/views/streamlit_ui.py
@@ -11,13 +11,10 @@ from prompt.defaults import (
 
 def run_api_key_ui():
     st.title("MOOI Project")
-
     openai_api_key = st.text_input("OpenAI API Key", type="password")
-
     if not openai_api_key:
         st.warning("API Key 입력 시 대화를 시작할 수 있습니다.")
         st.stop()
-
     os.environ["OPENAI_API_KEY"] = openai_api_key
 
 
