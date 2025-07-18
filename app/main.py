8825c8b6162cd38a5e1f44edec5683310ac9ad37
Jinhyuk-Yang <yangkyforever@gmail.com>
2025-07-19 00:28:49 +0900

    refactor: application layer

--- app/main.py
+++ app/main.py
@@ -1,29 +1,40 @@
-import sys
-
 from config import settings
 from repositories import StreamlitSessionRepository
 from services import ChatService
 
 
-def run_streamlit_app():
+def build_streamlit_service():
+    repo = StreamlitSessionRepository()
+    return ChatService(repo)
+
+
+def setup_streamlit_app(chat_service: ChatService, debug: bool = False):
     from views.streamlit_ui import run_chat_ui, run_api_key_ui, init_session
 
-    repo = StreamlitSessionRepository()
-    chat_service = ChatService(repo)
-    run_api_key_ui()
+    if debug:
+        from dotenv import load_dotenv
+
+        load_dotenv()
+    else:
+        run_api_key_ui()
+
     init_session(chat_service)
     run_chat_ui(chat_service)
+
+
+def run_streamlit_app():
+    chat_service = build_streamlit_service()
+    setup_streamlit_app(chat_service, debug=False)
 
 
 def run_streamlit_debug_app():
-    from views.streamlit_ui import run_chat_ui, init_session
-    from dotenv import load_dotenv
+    chat_service = build_streamlit_service()
+    setup_streamlit_app(chat_service, debug=True)
 
-    repo = StreamlitSessionRepository()
-    chat_service = ChatService(repo)
-    load_dotenv()
-    init_session(chat_service)
-    run_chat_ui(chat_service)
+
+# TODO: make FastAPI runnable
+def run_fastapi_app():
+    raise NotImplementedError("FastAPI mode is not implemented.")
 
 
 def main():
@@ -33,14 +44,9 @@ def main():
     elif mode == "streamlit":
         run_streamlit_app()
     elif mode == "fastapi":
-        print(f"[WARN] Not Implemented yet.")
-        sys.exit(0)
+        run_fastapi_app()
     else:
-        print(
-            f"[ERROR] Invalid APP_MODE: '{mode}'. Use 'streamlit' or 'fastapi'.",
-            file=sys.stderr,
-        )
-        sys.exit(1)
+        raise ValueError(f"Invalid APP_MODE: '{mode}'. Use 'streamlit' or 'fastapi'.")
 
 
 if __name__ == "__main__":
