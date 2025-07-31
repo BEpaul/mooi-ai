e6d49ea29b627b1aef4f1f075581af93f7644a15
Jinhyuk-Yang <yangkyforever@gmail.com>
2025-07-31 10:23:12 +0900

    refactor: split as dto

--- app/controllers/fastapi/app.py
+++ app/controllers/fastapi/app.py
@@ -1,20 +1,6 @@
-from pydantic import BaseModel
-
-from models import ChatSession
+from models import ChatSession, ChatRequest, ChatResponse
 from repositories import InMemoryChatSessionRepository
 from services import ChatService
-
-
-# TODO: separate DTO
-class ChatRequest(BaseModel):
-    session_id: str
-    user_input: str
-    chat_prompt_message: str = "너는 친절한 상담가야."
-
-
-# TODO: separate DTO
-class ChatResponse(BaseModel):
-    response: str
 
 
 def build_fastapi_service():
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,2 @@
+from .dtos import ChatRequest, ChatResponse
 from .entities import Chat, ChatSession, Gauge, TimeCapsule, TodaySentimentReportOutput
